7d98a103a67dc2b940906b31f131de418e1a7224
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-07-21 13:52:48 +0200

    Use "Lifting State Up" method to pass the data (not convenient when having too many levels)

--- lib/main.dart
+++ lib/main.dart
@@ -5,36 +5,47 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final String data = 'Top Secret Data!';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('A Title'),
+          title: Text(data),
         ),
-        body: Level1(),
+        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
+  final String data;
+  Level1(this.data);
+
   @override
   Widget build(BuildContext context) {
-    return Level2();
+    return Level2(data);
   }
 }
 
 class Level2 extends StatelessWidget {
+  final String data;
+  Level2(this.data);
+
   @override
   Widget build(BuildContext context) {
-    return Level3();
+    return Level3(data);
   }
 }
 
 class Level3 extends StatelessWidget {
+  final String data;
+  Level3(this.data);
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('A text'));
+    return Center(child: Text(data));
   }
 }
