f463432c43435bf5774848bec3baeb1a404d5b71
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-07-21 12:13:01 +0200

    Add starting point of project

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('A Title'),
+        ),
+        body: Level1(),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Level2();
+  }
+}
+
+class Level2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Level3();
+  }
+}
+
+class Level3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('A text'));
+  }
+}
