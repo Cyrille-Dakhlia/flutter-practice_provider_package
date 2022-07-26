ef605a80ffbcf6fc962cb0803b49987e962a1d64
Cyrille Dakhlia <dakhliacyrille@gmail.com>
2022-07-26 16:05:03 +0200

    Add "ChangeNotifier" to "Provider" package to pass the data and notify for any changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,26 @@ void main() {
   runApp(MyApp());
 }
 
+class MyDummyData with ChangeNotifier {
+  String data = 'Some initial data...';
+
+  void changeDataAndNotifyListeners(String newData) {
+    data = newData;
+    notifyListeners();
+  }
+}
+
 class MyApp extends StatelessWidget {
-  final String data = 'Some initial data...';
+  final MyDummyData data = MyDummyData();
 
   @override
   Widget build(BuildContext context) {
-    return Provider<String>(
+    return ChangeNotifierProvider<MyDummyData>(
       create: (BuildContext context) => data,
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text(data),
+            title: Text(data.data), // Note: this doesn't get updated
           ),
           body: Level1(),
         ),
@@ -34,33 +43,113 @@ class Level1 extends StatelessWidget {
 class Level2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Level3();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(child: Level3Bis()),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          margin: const EdgeInsets.only(bottom: 35.0),
+          child: TextField(
+            onChanged: (value) =>
+                Provider.of<MyDummyData>(context, listen: false)
+                    .changeDataAndNotifyListeners(value),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Level3Bis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(15.0),
+      children: [
+        TestProviderListening1(),
+        TestProviderNotListening1(),
+        TestProviderListening2(),
+        TestProviderNotListening2(),
+      ],
+    );
+  }
+}
+
+class TestProviderListening1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final valueProviderOf = Provider.of<MyDummyData>(context);
+    return DisplayColumn(
+        label: 'Provider.of<T>(context)', value: valueProviderOf.data);
+  }
+}
+
+class TestProviderListening2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Equivalent to 'Provider.of', but is accessible only inside [StatelessWidget.build] and [State.build].
+    final valueContextWatch = context.watch<MyDummyData>();
+    return DisplayColumn(
+        label: 'context.watch<T>()', value: valueContextWatch.data);
+  }
+}
+
+class TestProviderNotListening1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final valueProviderOfWithoutListening =
+        Provider.of<MyDummyData>(context, listen: false);
+    return DisplayColumn(
+        label: 'Provider.of<T>(context, listen: false)',
+        value: valueProviderOfWithoutListening.data);
+  }
+}
+
+class TestProviderNotListening2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final valueContextRead = context.read<MyDummyData>();
+    return DisplayColumn(
+        label: 'context.read<T>()', value: valueContextRead.data);
   }
 }
 
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final valueProviderOf = Provider.of<String>(context);
-    print('valueProviderOf = $valueProviderOf');
+    // WATCH OUT:
+    // using all of those methods below to retrieve MyDummyData in the same widget
+    // is not the right way to go, because the Level3 widget will be rebuild,
+    // rebuilding everything in it:
+    // - using "Provider.of<MyDummyData>(context)" and "context.watch<MyDummyData>()"
+    //   will trigger the rebuild of the Level3 widget because they are listening
+    //   to "class MyDummyData with ChangeNotifier"
+    // - which will remake the calls to "Provider.of<MyDummyData>(context, listen: false)
+    //   and "context.read<MyDummyData>()", causing them to display the updated value
+    //   instead of the initial value
+    final valueProviderOf = Provider.of<MyDummyData>(context);
+    print('valueProviderOf = ${valueProviderOf.data}');
 
     // Equivalent to 'Provider.of', but is accessible only inside [StatelessWidget.build] and [State.build].
-    final valueContextWatch = context.watch<String>();
-    print('valueContextWatch = $valueContextWatch');
+    final valueContextWatch = context.watch<MyDummyData>();
+    print('valueContextWatch = ${valueContextWatch.data}');
 
     final valueProviderOfWithoutListening =
-        Provider.of<String>(context, listen: false);
-    print('valueProviderOfWithoutListening = $valueProviderOfWithoutListening');
+        Provider.of<MyDummyData>(context, listen: false);
+    print(
+        'valueProviderOfWithoutListening = ${valueProviderOfWithoutListening.data}');
 
-    final valueContextRead = context.read<String>();
-    print('valueContextRead = $valueContextRead');
+    final valueContextRead = context.read<MyDummyData>();
+    print('valueContextRead = ${valueContextRead.data}');
 
     Map<String, String> map = {
-      'Provider.of<String>(context)': valueProviderOf,
-      'context.watch<String>()': valueContextWatch,
+      'Provider.of<String>(context)': valueProviderOf.data,
+      'context.watch<String>()': valueContextWatch.data,
       'Provider.of<String>(context, listen: false)':
-          valueProviderOfWithoutListening,
-      'context.read<String>()': valueContextRead
+          valueProviderOfWithoutListening.data,
+      'context.read<String>()': valueContextRead.data
     };
 
     return Center(
@@ -96,6 +185,7 @@ class DisplayColumn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           '$label:',
